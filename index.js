b9c1990ef9c08f7f5526d37b57f94fc50ef78622
arifvtti <arif.vtti@gmail.com>
2022-07-22 05:47:46 +0600

    setup mongodb

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const cors = require('cors')
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 
 
@@ -7,6 +8,17 @@ app.use(cors());
 app.use(express.json());
 
 const port = process.PORT || 5000;
+
+
+const uri = `mongodb+srv://${process.DB_USER}:${process.DB_PASS}@cluster0.ctfzx.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
+
 
 app.get("/", (req, res) => {
     res.send("Welcome to Life Care Server!");
