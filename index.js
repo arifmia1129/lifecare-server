f9bbd186550a982a94779e90424eca910698c7c1
arifvtti <arif.vtti@gmail.com>
2022-07-22 07:02:51 +0600

    doctors api added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require('cors');
+require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 
@@ -7,18 +8,29 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const port = process.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 
-const uri = `mongodb+srv://${process.DB_USER}:${process.DB_PASS}@cluster0.ctfzx.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ctfzx.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
 
+async function run() {
+    try {
+        await client.connect();
 
+        const doctorCollection = client.db("lifecare").collection("doctors");
+
+        app.get("/doctors", async (req, res) => {
+            const query = {};
+            const doctors = await doctorCollection.find(query).toArray();
+            res.send(doctors);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
     res.send("Welcome to Life Care Server!");
